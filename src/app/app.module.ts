3aaa820f83b05c79e76d949159f82463f53c59ae
glsantos <gabriel._.lima@hotmail.com>
2019-03-06 15:51:34 -0300

    improving category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { } from '';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { } from '';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -5,7 +5,7 @@ import { Category } from "./pages/categories/shared/category.model";
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
         const categories: Category[] = [
-            { id: 1, name: "Moraria", description: "Pagamentos de Contas da Casa" },
+            { id: 1, name: "Moradia", description: "Pagamentos de Contas da Casa" },
             { id: 2, name: "Saúde", description: "Plano de Sapude e Remédios" },
             { id: 3, name: "Lazer", description: "Cinema, parques, praia, etc" },
             { id: 4, name: "Salário", description: "Recebimento do Salário" },
